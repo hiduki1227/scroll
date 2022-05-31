67cd2e8144440e6c9d813b006762b5528d2623d5
hiduki_1227 <hidukipg@gmail.com>
2022-06-01 00:09:08 +0900

    main

--- script.js
+++ script.js
@@ -1,34 +1,39 @@
 console.log(location.href);
-function onclick_btn() {
+function clb() {
     const mus = "https://www.google.com/search?q=";
     var maiN = document.getElementById("main").value;
     var urL = document.getElementById("url").value;
-    var url
+    var url;
 
 
-    if (urL.length >= 0 & maiN > ) {
-        console.log("TRUE");
+    if (maiN.length > 0 & urL.length > 0 & urL != "過激" & urL != "k" & urL != "デフォルト" & urL != "d") {
+        console.log("キーワード：TRUE\nURL設定：TRUE");
         url = mus + maiN + " site:" + urL + "&tbm=isch";
         open(url, "_blank");
     }
 
-    else if (urL.length === 0 & maiN >= 0) {
-        console.log("FALSE");
+    else if (maiN.length > 0 & urL.length == 0) {
+        console.log("キーワード：TRUE\nURL設定：FALSE");
         url = mus + maiN + " site:http://moeimg.net" + " \"微エロ\"" + "&tbm=isch";
         open(url, "_blank");
     }
 
-    else if (urL === "指定なし" | urL === "n") {
+    else if (maiN.length > 0 & urL == "d") {
         url = mus + maiN + " site:http://moeimg.net" + "&tbm=isch";
         open(url, "_blank");
     }
 
-    else if (urL === "過激" | urL === "k") {
+    else if (maiN.length > 0 & urL == "k") {
         url = mus + maiN + " site:http://moeimg.net" + " \"エロ\" -微エロ -非エロ" + "&tbm=isch";
         open(url, "_blank");
     }
-    
-    else {
-        console.log("該当なし");
+
+    else if (maiN.length == 0 & urL.length == 0) {
+        console.log("無効な値");
     }
+
+    else {
+        console.log("無効なプログラム");
+    }
+    
 }
