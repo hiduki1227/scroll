dc4483baba7f782960c56455da45b56e20cb2de6
hiduki_1227 <hidukipg@gmail.com>
2023-03-24 00:59:41 +0900

    main

--- script.js
+++ script.js
@@ -1,54 +1,76 @@
 console.log(location.href);
 function clb() {
-    const mus = "https://www.google.com/search?q=";
+    // 必須URL
+    const mus = "https://www.google.com/search?tbm=isch&q=";
+
+    // 各要素内の値を取得
     var maiN = document.getElementById("main").value;
     var urL = document.getElementById("url").value;
     var img_w = document.getElementById("width").value;
     var img_h = document.getElementById("height").value;
+    var img_Px = document.getElementById("Impixel").value;
+    // 最終的に mus に結合するURL
     var url;
 
+
+    // 画像の横と縦の長さを指定。
     function img(iw, ih) {
         if (iw.length > 0 && ih.length > 0) {
-            url = url + " imagesize:" + iw + "x" + ih + "&tbm=isch";
+            var ImgSz = " imagesize:" + iw + "x" + ih;
         }
         else {
-            url = url + "&tbm=isch";
+            // pass
         }
+        return ImgSz;
     }
 
 
+    // 画像の画素数を指定
+    function Ipx(iP) {
+        var px;
+        if (iP > 0 && ((iP % 1) == 0)) {
+            if (iP <= 20) {
+                iP = iP;
+            }else {
+                iP = "20";
+            }
+            px = "&tbs=isz:lt,islt:" + iP;
+        } else {
+            px = "";
+        }
+        return px;
+    }
+
+
+    // 画像のキーワード設定
     if (maiN.length > 0 && urL.length > 0 && urL != "k" && urL != "d" && urL != "gimg") {
         console.log("キーワード：TRUE\nURL設定：TRUE");
-        url = mus + maiN + " site:" + urL;
-        img(img_w, img_h);
+        url = mus + maiN + " site:" + urL + img(img_w, img_h) + Ipx(img_Px);
         open(url, "_blank");
     }
 
     else if (maiN.length > 0 && urL.length == 0) {
         console.log("キーワード：TRUE\nURL設定：FALSE");
-        url = mus + maiN + " site:http://moeimg.net" + " \"微エロ\"";
-        img(img_w, img_h);
+        url = mus + maiN + " site:http://moeimg.net" + " \"微エロ\"" + img(img_w, img_h) + Ipx(img_Px);
         open(url, "_blank");
     }
 
     else if (maiN.length > 0 && urL == "d") {
-        url = mus + maiN + " site:http://moeimg.net";
-        img(img_w, img_h);
+        url = mus + maiN + " site:http://moeimg.net" + img(img_w, img_h) + Ipx(img_Px);
         open(url, "_blank");
     }
 
     else if (maiN.length > 0 && urL == "k") {
-        url = mus + maiN + " site:http://moeimg.net" + " \"エロ\" -微エロ -非エロ";
-        img(img_w, img_h);
+        url = mus + maiN + " site:http://moeimg.net" + " \"エロ\" -微エロ -非エロ" + img(img_w, img_h) + Ipx(img_Px);
         open(url, "_blank");
     }
 
-    else if (maiN.length > 0 && urL == "gimg") {
-        url = mus + maiN;
-        img(img_w, img_h);
+    else if (maiN.length > 0 && urL == "g") {
+        url = mus + maiN + img(img_w, img_h) + Ipx(img_Px);
         open(url, "_blank");
     }
 
+    // 無効な入力
     else if (maiN.length == 0 && urL.length == 0) {
         console.log("無効な値");
         alert("無効な値です\nKeywordsを入力してください");
